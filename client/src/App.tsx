ce9a42ade6a290291f6f5cd65e4d0eb9d04d3cb5
DayColor <2005alonlivne@gmail.com>
2020-04-03 16:13:59 +0300

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import Contacts from './components/Contacts';
+
+function App() {
+  return (
+    <Grid container direction="row" justify="flex-start" alignItems="center">
+      <Grid item xs={3}>
+        <Contacts />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default App;
---
+++ client/src/components/Contacts.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../scss/contacts.scss';
+import { Box } from '@material-ui/core';
+
+const Contacts = () => {
+  return (
+    <Box minHeight="100vh">
+
+    </Box>
+  );
+};
+
+export default Contacts;
